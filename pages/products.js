6904e6a1a0c19734f2ed15122bd21bcec3bcf489
Leoozito <leoanrdonogueira616@gmail.com>
2022-10-07 16:42:25 -0300

    feat: :sparkles: Mudança tela de produto

--- pages/products.js
+++ pages/products.js
@@ -5,25 +5,6 @@ export default function Products() {
     return (
         <>
         <h1>Página de Produtos</h1>
-        <div className={styles.imgleft}>
-            <p>Prensa Hidraulica para Placas Mercosul</p>
-            <Image src="/../public/foto-prensa.png" width="100" height="200" alt="prensa hidráulica"/>
-        </div>
-            
-        <div className={styles.imgright}>
-            <p>Matrizes Alfanúmericas Mercosul</p>
-            <Image src="/../public/Matrizes.png" width="200" height="200" alt="Matrizes Alfanúmericas"/>
-        </div>
-            
-        <div className={styles.imgleftdown}>
-            <p>HotStamp para Placas Mercosul</p>
-            <Image src="/../public/HotStamp.png" width="200" height="200" alt="HotStamp"/>
-        </div>
-            
-        <div className={styles.imgrightdown}>
-        <p>Prensa Hidráulica Inteligente</p>
-            <Image src="/../public/prensa-inteligente.png" width="100" height="200" alt="prensa inteligente"/>
-        </div>
         </>
     )
 }
